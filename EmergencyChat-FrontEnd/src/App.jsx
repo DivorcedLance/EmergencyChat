e29583659816c2e1f17bd4fc97bb8fbd20e8919e
DivorcedLance <divorcedlance@gmail.com>
2024-03-07 22:54:40 -0500

    Changing App

--- EmergencyChat-FrontEnd/src/App.jsx
+++ EmergencyChat-FrontEnd/src/App.jsx
@@ -1,16 +1,12 @@
 import './App.css'
-import { DataManager } from './components/DataManager'
-
 import LoginSign from './components/LoginSign'
-import TestComponent from './components/TestComponent'
-import {DataManager} from './components/DataManager'
 
 function App() {
   
 
   return (
     <>
-      <DataManager />
+      <LoginSign />
     </>
   )
 }
